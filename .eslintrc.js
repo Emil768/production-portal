480dc34558f0a0da036b0d544e5bd2df7060646b
Emil Murahas <murakhas@list.ru>
2024-01-31 12:03:31 +0300

    initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,52 @@
+module.exports = {
+	env: {
+		browser: true,
+		es2021: true,
+		jest: true,
+	},
+	extends: ['plugin:react/recommended', 'airbnb', 'plugin:i18next/recommended'],
+	parser: '@typescript-eslint/parser',
+	parserOptions: {
+		ecmaFeatures: {
+			jsx: true,
+		},
+		ecmaVersion: 'latest',
+		sourceType: 'module',
+	},
+	plugins: ['react', '@typescript-eslint', 'i18next'],
+	rules: {
+		indent: 'off',
+		'max-len': ['warn', 180, 2, { ignoreUrls: true, ignorePattern: '^import' }],
+		'react/jsx-indent': 'off',
+		'react/jsx-indent-props': 'off',
+		'object-curly-newline': 'off',
+		'react/jsx-filename-extension': [2, { extensions: ['.js', '.jsx', '.tsx'] }],
+		'import/no-unresolved': 'off',
+		'import/prefer-default-export': 'off',
+		'no-unused-vars': 'warn',
+		'react/require-default-props': 'off',
+		'react/react-in-jsx-scope': 'off',
+		'react/jsx-props-no-spreading': 'warn',
+		'react/function-component-definition': 'off',
+		'no-shadow': 'off',
+		'import/extensions': 'off',
+		'import/no-extraneous-dependencies': 'off',
+		'no-underscore-dangle': 'off',
+		'no-tabs': 'off',
+		'linebreak-style': 'off',
+		'arrow-body-style': 'off',
+		'i18next/no-literal-string': ['error', { markupOnly: true, ignoreAttribute: ['data-testid'] }],
+		'react/button-has-type': ['error'],
+	},
+	globals: {
+		__IS_DEV__: true,
+	},
+	overrides: [
+		{
+			files: ['**/src/**/*.test.{ts,tsx}'],
+			rules: {
+				'i18next/no-literal-string': 'off',
+			},
+		},
+	],
+};
---
+++ src/shared/lib/delayDemo/delayDemo.ts
@@ -0,0 +1,5 @@
+export function delayForDemo<T>(promise: Promise<T>): Promise<T> {
+	return new Promise((resolve) => {
+		setTimeout(resolve, 800);
+	}).then(() => promise);
+}
