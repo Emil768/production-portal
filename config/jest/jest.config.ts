f7dd598ee5c0f7d69d674b74b44842e3cd54dfb9
Emil Murahas <murakhas@list.ru>
2024-02-09 11:23:09 +0300

    Update jest.config.ts

--- config/jest/jest.config.ts
+++ config/jest/jest.config.ts
@@ -19,6 +19,7 @@ export default {
 		// Обнаружил разницу между МАК ОС и ВИНДОУС!!!
 		'<rootDir>src/**/*(*.)@(spec|test).[tj]s?(x)',
 	],
+	transformIgnorePatterns: ['node_modules/(?!axios)'],
 	rootDir: '../../',
 	setupFilesAfterEnv: ['<rootDir>config/jest/setupTests.ts'],
 	moduleNameMapper: {
